459a46c233f592e830730e2ee8ea594d7cf18db3
JIMENEZ HERNANDEZ <jonathan.jimenez-hernandez@capgemini.com>
2020-11-30 23:12:40 -0600

    version 1

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,22 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'practicasBasicasPipes';
-}
+title='practicasBasicasPipes';
+name='Sheldon';
+myArray=[1,2,3,4,5,6,7,8];
+PI=Math.PI;
+myNum=0.589;
+salary=3500.5;
+nerd={
+  name:'Sheldon',
+  alias:'Shelly',
+  song:'Soft Kitty',
+  skills: ['eidetic memory', 'makes peoples nervous'],
+  youtubeChannel:'Fun With Flags',
+  address:{ street: 'Elm Street',
+number: 3,
+city: 'Pasadena'}};
+myDate=new Date();
+video='o-0ygW-B_gI';
+
+   }​​
